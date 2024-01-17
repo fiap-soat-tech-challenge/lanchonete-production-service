fed72c8bb32beeb27208ec65a4a527ca285252c3
Jonilson Sousa <jonilsonds9@gmail.com>
2024-01-17 09:10:19 -0300

    Removendo coisas desnecessarias do servico

--- mock_pagamento/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import pagamento from './pagamento/index.js';
-
-var routes = Router();
-
-routes.use('/pagamento', pagamento);
-
-export default routes;
--- mock_pagamento/src/routes/pagamento/getPagamento.js
+++
@@ -1,7 +0,0 @@
-export default async (request, response) => {
-  response.send({
-    id: request.params.id,
-    status: 'pagamento confirmado',
-    codigo: 200,
-  });
-};
