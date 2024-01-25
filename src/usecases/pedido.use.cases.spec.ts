5adbac099003bb527d69938430bdca33c9bbdbfd
Jonilson Sousa <jonilsonds9@gmail.com>
2024-01-24 23:18:40 -0300

    Arrumando testes

---
+++ src/usecases/pedido.use.cases.spec.ts
@@ -0,0 +1,7 @@
+describe('PedidoUseCases', () => {
+  describe('getAllPedidos', () => {
+    it('should return an array of pedidos', async () => {
+      expect(true).toEqual(true);
+    });
+  });
+});
