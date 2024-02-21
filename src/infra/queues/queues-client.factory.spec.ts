fa05191fe81540ad3420713db9098d59323b1b91
Jonilson Sousa <jonilsonds9@gmail.com>
2024-02-21 18:45:48 -0300

    Fix rabbitmq url

--- src/infra/queues/queues-client.factory.spec.ts
+++ src/infra/queues/queues-client.factory.spec.ts
@@ -4,47 +4,13 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { instance, mock, when } from 'ts-mockito';
 
 describe('ApprovedPaymentsFactory', () => {
-  let approvedPaymentsFactory: QueuesClientFactory;
+  let factory: QueuesClientFactory;
   let configService: ConfigService;
 
-  beforeEach(async () => {
-    process.env.QUEUE_USER = 'user';
-    process.env.QUEUE_PASSWORD = 'password';
-    process.env.QUEUE_HOST = 'localhost';
-    process.env.QUEUE_PORT = '5672';
-
-    configService = mock(ConfigService);
-    when(configService.get('QUEUE_USER')).thenReturn(process.env.QUEUE_USER);
-    when(configService.get('QUEUE_PASSWORD')).thenReturn(
-      process.env.QUEUE_PASSWORD,
-    );
-    when(configService.get('QUEUE_HOST')).thenReturn(process.env.QUEUE_HOST);
-    when(configService.get('QUEUE_PORT')).thenReturn(
-      parseInt(process.env.QUEUE_PORT),
-    );
-
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [
-        QueuesClientFactory,
-        { provide: ConfigService, useValue: instance(configService) },
-      ],
-    }).compile();
-
-    approvedPaymentsFactory = module.get<QueuesClientFactory>(
-      QueuesClientFactory,
-    );
-  });
-
-  it('should be defined', () => {
-    expect(approvedPaymentsFactory).toBeDefined();
-  });
-
-  it('should create module config', async () => {
-    const result = await approvedPaymentsFactory.createModuleConfig();
-
-    expect(result).toEqual({
+  function getExpectedResult() {
+    return {
       name: 'RabbitMQ Server',
-      uri: `amqp://${process.env.QUEUE_USER}:${process.env.QUEUE_PASSWORD}@${process.env.QUEUE_HOST}:${process.env.QUEUE_PORT}`,
+      uri: `amqp://${process.env.QUEUE_USER}:${process.env.QUEUE_PASSWORD}@localhost:5672`,
       queues: [
         {
           name: 'pagamentos_aprovados',
@@ -59,6 +25,78 @@ describe('ApprovedPaymentsFactory', () => {
           },
         },
       ],
+    };
+  }
+
+  beforeEach(async () => {
+    configService = mock(ConfigService);
+
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        QueuesClientFactory,
+        { provide: ConfigService, useValue: instance(configService) },
+      ],
+    }).compile();
+
+    factory = module.get<QueuesClientFactory>(QueuesClientFactory);
+  });
+
+  it('should be defined', async () => {
+    process.env.QUEUE_USER = 'user';
+    process.env.QUEUE_PASSWORD = 'password';
+    process.env.QUEUE_HOST = 'localhost';
+
+    when(configService.get('QUEUE_USER')).thenReturn(process.env.QUEUE_USER);
+    when(configService.get('QUEUE_PASSWORD')).thenReturn(
+      process.env.QUEUE_PASSWORD,
+    );
+    when(configService.get('QUEUE_URI')).thenReturn(process.env.QUEUE_URI);
+
+    expect(factory).toBeDefined();
+  });
+
+  describe('createClientOptions', () => {
+    it('should create client options', async () => {
+      process.env.QUEUE_USER = 'user';
+      process.env.QUEUE_PASSWORD = 'password';
+      process.env.QUEUE_URI = 'amqp://localhost:5672';
+
+      when(configService.get('QUEUE_USER')).thenReturn(process.env.QUEUE_USER);
+      when(configService.get('QUEUE_PASSWORD')).thenReturn(
+        process.env.QUEUE_PASSWORD,
+      );
+      when(configService.get('QUEUE_URI')).thenReturn(process.env.QUEUE_URI);
+
+      const result = await factory.createModuleConfig();
+
+      expect(result).toEqual(getExpectedResult());
     });
+
+    it('should create client options false', async () => {
+      process.env.QUEUE_USER = 'user';
+      process.env.QUEUE_PASSWORD = 'password';
+      process.env.QUEUE_HOST = 'localhost';
+      process.env.QUEUE_PORT = '5672';
+
+      when(configService.get('QUEUE_USER')).thenReturn(process.env.QUEUE_USER);
+      when(configService.get('QUEUE_PASSWORD')).thenReturn(
+        process.env.QUEUE_PASSWORD,
+      );
+      when(configService.get('QUEUE_HOST')).thenReturn(process.env.QUEUE_HOST);
+      when(configService.get('QUEUE_PORT')).thenReturn(
+        parseInt(process.env.QUEUE_PORT),
+      );
+
+      const result = await factory.createModuleConfig();
+
+      expect(result).toEqual(getExpectedResult());
+    });
+  });
+
+  afterEach(() => {
+    delete process.env.QUEUE_USER;
+    delete process.env.QUEUE_PASSWORD;
+    delete process.env.QUEUE_HOST;
+    delete process.env.QUEUE_PORT;
   });
 });
